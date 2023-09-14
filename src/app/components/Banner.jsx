345d814a4600fa70913cdd82163bb33b0bbce095
Elvio <elvio.galeano@gmail.com>
2023-09-14 00:23:17 -0300

    se centro la frase

--- src/app/components/Banner.jsx
+++ src/app/components/Banner.jsx
@@ -5,7 +5,7 @@ export default function Banner() {
     <div className="bg-[url('https://wallpapercave.com/wp/wp2445596.jpg')] bg-no-repeat flex items-center justify-center h-[calc(100vh)] bg-cover ">
       <div className="flex flex-col items-center">
         <img className="w-96 opacity-0 animate-fade-in" src="https://muplatas2.com/images/logo.png" />
-        <h2 className="text-slate-100 font-bold pt-10 text-3xl">¡Forja tu leyenda en un mundo de aventuras y batallas épicas en Mu Online!</h2>
+        <h2 className="text-slate-100 font-bold pt-10 text-3xl text-center">¡Forja tu leyenda en un mundo de aventuras y batallas épicas en Mu Online!</h2>
       </div>
     </div>
   );
